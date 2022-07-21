2856fe15f28b4dd7bfdbce3ec1eecdca1bdc10e7
SamuelKer <samuel@onlydust.xyz>
2022-07-21 11:49:50 +0200

    remove menu / replace starknetd by starknet

--- install.sh
+++ install.sh
@@ -101,8 +101,7 @@ echo "==================================================="
 echo -e '\n\e[42mCheck node status\e[0m\n' && sleep 1
 if [[ `service starknet status | grep active` =~ "running" ]]; then
   echo -e "Your StarkNet node \e[32minstalled and works\e[39m!"
-  echo -e "You can check node status by the command \e[7mservice starknetd status\e[0m"
-  echo -e "Press \e[7mQ\e[0m for exit from status menu"
+  echo -e "You can check node status by the command \e[7mservice starknet status\e[0m"
 else
   echo -e "Your StarkNet node \e[31mwas not installed correctly\e[39m, please reinstall."
 fi
